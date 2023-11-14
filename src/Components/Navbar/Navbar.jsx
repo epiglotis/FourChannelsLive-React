ae0c0c014125ea68ffc6df3e8de0205bee63dd1d
epiglotis <83986620+epiglotis@users.noreply.github.com>
2023-11-14 19:44:07 +0300

    Implementing link functionality

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ResetScreen from '../ResetScreen/ResetScreen';
 import DarkSwitch from '../DarkSwitch/DarkSwitch';
 
-const Navbar = () => {
+const Navbar = ({ getVideoLink }) => {
+  const [videoLink, setVideoLink] = useState('');
+
+  useEffect(() => console.log(videoLink), [videoLink]);
+
+  getVideoLink = getVideoLink = () => {
+    return videoLink;
+  };
+
   return (
     <div className='flex w-full justify-between'>
       <ResetScreen />
-      <text>Canlı Yayın Takip</text>
+      <div className='flex gap-10 w-1 justify-center'>
+        <input
+          className='bg-transparent text-center border border-cyan-600'
+          placeholder='Youtube Linki Ekleyin'
+          type='url'
+          onChange={(e) => setVideoLink(e.target.value)}
+        ></input>
+        <button>Send</button>
+      </div>
       <DarkSwitch />
     </div>
   );
